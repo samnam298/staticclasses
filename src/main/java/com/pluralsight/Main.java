32f53ca938899a863ca117c9bbe6809ec8064a21
samnam298 <samrawithailena@gmail.com>
2025-10-22 13:36:35 -0400

    can format names in a standard way

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(NameFormatter.format("Mel", "Johnson"));
+        System.out.println(NameFormatter.format("Dr.", "Mel", "B", "Johnson", "PhD"));
+        System.out.println(NameFormatter.format("Dr. Mel B Johnson, PhD"));
+        System.out.println(NameFormatter.format("Mel Johnson"));
+    }
 }
